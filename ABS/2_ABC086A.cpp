4c0d5856a605c590c6c172f9131515bf627f4d96
Teruchika Ishihara <terutika0604@gmail.com>
2025-05-07 23:23:24 +0900

    ABS ~3

---
+++ ABS/2_ABC086A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  int product = a * b;
+
+  if (product % 2 == 0) {
+    cout << "Even" << endl;
+  } else {
+    cout << "Odd" << endl;
+  }
+
+  return 0;
+}
