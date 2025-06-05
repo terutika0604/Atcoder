b80f09983cb3592b943b9f0dcd364de2430bbf3f
Teruchika Ishihara <terutika0604@gmail.com>
2025-06-05 22:52:32 +0900

    整理整頓

---
+++ AOJ/ALDS1/10/A.cpp
@@ -0,0 +1,9 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, start, end) for (int i = (int)(start); i < (int)(end); i++)
+using ll = long long;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+int main() {}
